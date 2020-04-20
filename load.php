c0d80d62987d4b13482f76d085d25fa25c6d9ca6
deepak5627 <30997503+deepak5627@users.noreply.github.com>
2020-04-20 11:09:53 +0530

    Update load.php

--- load.php
+++ load.php
@@ -1,19 +1,19 @@
 
 <?php
 
-
-    if ( 0 < $_FILES['file']['error'] ) {
+  if ( 0 < $_FILES['file']['error'] ) {
         echo 'Error: ' . $_FILES['file']['error'] . '<br>';
     }
     else {
 	    $fileName = $_FILES['file']['name'];
-	   
+	       
         move_uploaded_file($_FILES['file']['tmp_name'], 'upload/' . $_FILES['file']['name']);
        
-    	$conn = mysqli_connect("localhost","root", "", "fileupload");
-    $query = "INSERT INTO filedata(fileName) VALUES('$fileName')";
-    $data = mysqli_query($conn, $query);
-    }
+    	$conn = mysqli_connect("sql3.freemysqlhosting.net","sql3334623", "xniRV9R8VN", "sql3334623");
+        $query = "INSERT INTO project(image) VALUES('$fileName')";
+        $data = mysqli_query($conn, $query);
+        }
+
 
 
 ?>
